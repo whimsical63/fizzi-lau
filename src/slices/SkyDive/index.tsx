7110de53ec86acdc43f113fa4ec556afa64651b4
L A <laurenceunabia@gmail.com>
2025-01-15 21:10:52 +0800

    update and removed cloud errors

--- src/slices/SkyDive/index.tsx
+++ src/slices/SkyDive/index.tsx
@@ -16,19 +16,22 @@ export type SkyDiveProps = SliceComponentProps<Content.SkyDiveSlice>;
  */
 const SkyDive = ({ slice }: SkyDiveProps): JSX.Element => {
   return (
-    <Bounded
-      data-slice-type={slice.slice_type}
-      data-slice-variation={slice.variation}
-      className="skydive h-screen"
-    >
-      <h2 className="sr-only">{slice.primary.sentence}</h2>
-      <View className="h-screen w-screen">
-        <Scene
-          flavor={slice.primary.flavor}
-          sentence={slice.primary.sentence}
-        />
-      </View>
-    </Bounded>
+      <div>
+
+      </div>
+    // <Bounded
+    //   data-slice-type={slice.slice_type}
+    //   data-slice-variation={slice.variation}
+    //   className="skydive h-screen"
+    // >
+    //   <h2 className="sr-only">{slice.primary.sentence}</h2>
+    //   <View className="h-screen w-screen">
+    //     <Scene
+    //       flavor={slice.primary.flavor}
+    //       sentence={slice.primary.sentence}
+    //     />
+    //   </View>
+    // </Bounded>
   );
 };
 
